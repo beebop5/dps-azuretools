bbeb6b50f12c32c28bbbf19c716f19bb503645c3
DUNCAN <dsmith@dps-i.net>
2019-10-18 01:30:24 +0000

    Comments added

--- azure-function-core-tools.sh
+++ azure-function-core-tools.sh
@@ -1,4 +1,6 @@
 #Run as a pipeline task for installing azure-function-core-tools
+#This will deploy the latest version of the core tolls to the agent each time the pipline runs
+# ***beware of behavior changes between core tolls versions***
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
 
